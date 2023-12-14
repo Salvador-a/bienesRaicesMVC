9182615d66f22e9437464df93d5a0637be3d6017
Salvador-a <salvador.sar614@outlook.com>
2023-12-14 13:35:42 -0600

    Se creo el controlador para los vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController {
+    public static function crear() {
+        echo "crear Vendedor";
+    }
+
+    public static function actualizar() {
+        echo "Actualizar Vendedor";
+    }
+
+    public static function creliminarear() {
+        echo "Eliminar Vendedor";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\PropiedadController;
+use Controllers\VendedorController;
 
 $router = new Router();
 
@@ -15,6 +16,12 @@ $router->post('/propiedades/crear', [PropiedadController::class, 'crear' ]);
 $router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar' ]);
 $router->post('/propiedades/actualizar', [PropiedadController::class, 'actualizar' ]);
 $router->post('/propiedades/eliminar', [PropiedadController::class, 'eliminar' ]);
+
+$router->get('/vendedor/crear', [VendedorController::class, 'crear' ]);
+$router->post('/vendedor/crear', [VendedorController::class, 'crear' ]);
+$router->get('/vendedor/actualizar', [VendedorController::class, 'actualizar' ]);
+$router->post('/vendedor/actualizar', [VendedorController::class, 'actualizar' ]);
+$router->post('/vendedor/eliminar', [VendedorController::class, 'eliminar' ]);
 
 
 $router->comprobarRutas();
