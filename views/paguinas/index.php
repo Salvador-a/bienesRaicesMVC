a7c148c87de1ebbbe97f4be8415e8d20e3881784
Salvador-a <salvador.sar614@outlook.com>
2023-12-14 18:52:41 -0600

    Ligero cambio pero un con errore en la imagen

--- views/paguinas/index.php
+++ views/paguinas/index.php
@@ -1,41 +1,50 @@
+
 <main class="contenedor seccion">
     <h1>Más Sobre Nosotros</h1>
 
     <div class="iconos-nosotros">
         <div class="icono">
             <img src="build/img/icono1.svg" alt="Icono seguridad" loading="lazy">
-            <h3>Seguridad</h3>
-            <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+            <h3>Segiridad</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste doloremque accusamus quod tempore veniam aperiam consequuntur </p>
+
         </div>
         <div class="icono">
             <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
             <h3>Precio</h3>
-            <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste doloremque accusamus quod tempore veniam aperiam consequuntur </p>
+
         </div>
         <div class="icono">
             <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
-            <h3>A Tiempo</h3>
-            <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+            <h3>Seguridad</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste doloremque accusamus quod tempore veniam aperiam consequuntur</p>
+
         </div>
+
     </div>
 </main>
 
+
 <section class="seccion contenedor">
-    <h2>Casas y Depas en Venta</h2>
+        <h2>Casas y Depas en Venta</h2>
 
-    <?php 
-        include 'listado.php';
-    ?>
+        <?php 
+            include 'listado.php';
+        ?>
 
-    <div class="alinear-derecha">
-        <a href="/propiedades" class="boton-verde">Ver Todas</a>
-    </div>
-</section>
+
+        
+
+        <div class="alinear-derecha">
+            <a href="anuncios.php" class="boton-verde">Ver Todas</a>
+        </div>
+    </section>
 
 <section class="imagen-contacto">
     <h2>Encuentra la casa de tus sueños</h2>
     <p>Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-    <a href="/contacto" class="boton-amarillo">Contactános</a>
+    <a href="contacto.php" class="boton-amarillo">Contactános</a>
 </section>
 
 <div class="contenedor seccion seccion-inferior">
@@ -92,7 +101,8 @@
             <blockquote>
                 El personal se comportó de una excelente forma, muy buena atención y la casa que me ofrecieron cumple con todas mis expectativas.
             </blockquote>
-            <p>- Juan De la torre</p>
+            <p>- Salvador Acosta</p>
         </div>
     </section>
 </div>
+
