986cfe4251b33e2f1a09f6c4e8bb24102002dc8f
Salvador-a <salvador.sar614@outlook.com>
2023-12-15 18:48:54 -0600

    Se implemento los ultimos cambios

--- Router.php
+++ Router.php
@@ -18,15 +18,32 @@ namespace MVC;
 
 
     public function comprobarRutas()  {
+
+        session_start();
+
+        $auth = $_SESSION[ 'login'] ?? null;
+
+
+        // Arreglo de rutas proptegidas..
+        $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', 'vendedores/actualizar', '/vendedores/eliminar'];
+
+        
+
        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
        $metodo = $_SERVER['REQUEST_METHOD'] ;
 
+       
+
        if ($metodo === 'GET') {
-            $fn= $this->rutasGET[$urlActual] ?? null;
-           
+            $fn= $this->rutasGET[$urlActual] ?? null;           
        }else {
-            
             $fn= $this->rutasPOST[$urlActual] ?? null;
+       }
+
+       // Protege  las rutas
+       if (in_array($urlActual, $rutas_protegidas) && !$auth) {
+            header('Location: /');
+        
        }
 
        if ($fn) {
